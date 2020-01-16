4e0fb4a8e2455041a72f18ec32492cc2ce93f442
Karan <kumarkay.ca@gmail.com>
2020-01-16 17:15:09 -0500

    Initial Commit

---
+++ iOS_Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Data
+//
+//  Created by MacStudent on 2020-01-16.
+//  Copyright © 2020 Karan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
